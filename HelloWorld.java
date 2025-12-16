cab1f42dfd945576f9f7b1737aa7ec2d3b47cda9
zzzerkkk <riwajshrstha@gmail.com>
2025-12-15 22:57:49 -0500

    summary of changes
    modified and renamed the program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello world!");
+      System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
+   }
+}
--- hello-world.java
+++
@@ -1,7 +0,0 @@
-public class hello-world
-{
-   public static void main(String[] args)
-   {
-      System.out.println("Hello world!");
-   }
-}
